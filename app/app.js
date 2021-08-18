61c0b0c90ef77e99b41654c43cce7eddae1002c4
Sebastian <SCsebastian1406@gmail.com>
2021-08-18 13:56:14 -0500

    corregido animacion

--- app/app.js
+++ app/app.js
@@ -2,9 +2,18 @@ let botonClose = document.querySelector(".nav__imagen-close");
 let botonHamburger = document.querySelector(".nav__imagen-hamburger");
 
 const desplegar = () => {
-    document.querySelector(".nav__mobile").classList.toggle("nav__mobile-activo");
-    document.querySelector(".nav__mobile-enlace").style.animation = "aparecer 1s";
+    let nav = document.querySelector(".nav__mobile");
+    let navEnlace = document.querySelector(".nav__mobile-enlace")
+    nav.classList.toggle("nav__mobile-activo");
+
+    if(nav.classList[1] == "nav__mobile-activo"){
+        navEnlace.style.animation = "aparecer 1s";
+    }else{
+        navEnlace.style.animation = "inherit";
+    }
+    
 }
+
 
 botonClose.addEventListener("click", desplegar);
 botonHamburger.addEventListener("click", desplegar);
